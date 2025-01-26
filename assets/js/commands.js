4002462412d0dabf64f083484602845ed3479d60
Flor Stellamans <flor.stellamans@student.howest.be>
2025-01-26 11:36:13 +0100

    create the list of commands and what to show

---
+++ assets/js/commands.js
@@ -0,0 +1,56 @@
+"use strict";
+
+
+export const help = [
+    "<br></br>",
+    "help           show the list of commands", 
+    "whois          who am I",
+    "projects       my projects",
+    "socials        show socials",
+    "anime          show favorite animes",         
+    "clear          clear the terminal"
+];
+
+export const whois = [
+    "<br></br>",
+    "My name is Flor stellamans and I am 22 years old.",
+    "I grauated collage at Howest in Bruges with the diploma of Applied computer sciense",
+    "I spend most of my free time watching anime, working out, reading books and look at the crypto market.",
+    "What I also like to do is take walks, meditate and writing code.",
+];
+
+export const project = [
+    "<br></br>",
+    "Here is a list of all the projects I made",
+    "- Motivation of the day",
+    "       This is a browser extension that shows a quote, and a video of motivation",
+    "- Rename script",
+    "       This script lets you rename stremio downloads",
+    "- Terminal website",
+    "       This is the website you are looking at :)",
+    "<br></br>",
+    "You can always look at my Github if you like",
+    "<a href='https://github.com/xMelow'>GitHub</a>"
+];
+
+export const socials = [
+    "<br></br>",
+    "Linkedin           <a href='https://www.linkedin.com/in/flor-stellamans-4b901a19a/'>Linkedin</a>",
+    "Instrgram          <a href='#'>Instragram</a>",
+];
+
+export const anime = [
+    "<br></br>",
+    "My top 3 best anime are:",
+    "1. One piece",
+    "       I think this was very ovious and every one that has watched one piece will agree with me",
+    "2. Jujutsu kaisen",
+    "       Why I liked JJK so mutch where the characters I really like Itadori, Todo and Nanami",
+    "       The story was faced paced and action packt so I was constently on the tip of my seat when watching",
+    "3. Grand blue",
+    "       This is one you probebly haven't see but is a must watch",
+    "       This is a hilarious commedy and trust me its about diving ;)",
+    "<br></br>",
+    "If you are intrested in my entire list you can check it out on anilist",
+    "<a href='https://anilist.co/user/fstellam/'>Anilist</a>"
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
 "use strict";
 
+import { help } from './commands.js'
+
+function command(cmd) {
+    switch(cmd.toLowerCase()) {
+        case 'help':
+            help;
+            break;
+        case 'whois':
+            whois;
+            break;
+        case 'projects':
+            projects;
+            break;
+        case 'socials':
+            socials;
+            break;
+        case 'clear':
+            //Clear the terminal
+            break;
+        default:
+            //Error message
+            break;
+    }
+}
